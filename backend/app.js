fc34476047d966690db9662bab5b44f69c2c6fe4
Kseniya Mudrakova <kseniyamudrakovadev@gmail.com>
2021-10-29 15:28:38 +0200

    helmet remove

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -17,15 +16,14 @@ mongoose.connect('mongodb+srv://ksenia:nrppgt@cluster0.tlcbj.mongodb.net/Project
 
 
 const app = express();
-app.use(cors());
-/*
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-*/
+
 
 app.use(express.json());
 
