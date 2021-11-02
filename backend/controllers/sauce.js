7ddff7ec973b2f7fa12273b6370f82e9c7eae83f
Kseniya Mudrakova <kseniyamudrakovadev@gmail.com>
2021-11-02 20:11:24 +0100

    add likedislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -98,9 +98,104 @@ exports.getAllSauce = (req, res, next) => {
     );
 };
 
-exports.likedSauce = (req, res, next) => {
-    let like = req.body.like;
-    let userId = req.body.userId;
-    let sauceID = req.params.id
+// Like and Dislike part
 
-};
+exports.likeDislike = (req, res, next) => {
+    let like = req.body.like;
+    let sauceID = req.params.id;
+    let userId = req.body.userId;
+
+    switch (like) {
+        case 1:
+            Sauce.updateOne({
+                    _id: sauceID
+                }, {
+                    $push: {
+                        usersLiked: userId
+                    },
+                    $inc: {
+                        likes: +1
+                    }
+                })
+                .then(() => res.status(200).json({
+                    message: `Votre like a été ajouté`
+                }))
+                .catch((error) => res.status(400).json({
+                    error
+                }))
+
+            break;
+
+        case 0:
+            Sauce.findOne({
+                    _id: sauceID
+                })
+                .then((sauce) => {
+                    if (sauce.usersLiked.includes(userId)) {
+                        Sauce.updateOne({
+                                _id: sauceID
+                            }, {
+                                $pull: {
+                                    usersLiked: userId
+                                },
+                                $inc: {
+                                    likes: -1
+                                }
+                            })
+                            .then(() => res.status(200).json({
+                                message: `like annulé`
+                            }))
+                            .catch((error) => res.status(400).json({
+                                error
+                            }))
+                    }
+                    if (sauce.usersDisliked.includes(userId)) {
+                        Sauce.updateOne({
+                                _id: sauceID
+                            }, {
+                                $pull: {
+                                    usersDisliked: userId
+                                },
+                                $inc: {
+                                    dislikes: -1
+                                }
+                            })
+                            .then(() => res.status(200).json({
+                                message: `dislike annulé`
+                            }))
+                            .catch((error) => res.status(400).json({
+                                error
+                            }))
+                    }
+                })
+                .catch((error) => res.status(404).json({
+                    error
+                }))
+            break;
+
+        case -1:
+            Sauce.updateOne({
+                    _id: sauceID
+                }, {
+                    $push: {
+                        usersDisliked: userId
+                    },
+                    $inc: {
+                        dislikes: +1
+                    }
+                })
+                .then(() => {
+                    res.status(200).json({
+                        message: `Dislike ajouté`
+                    })
+                })
+                .catch((error) => res.status(400).json({
+                    error
+                }))
+            break;
+
+        default:
+            console.log(error);
+    }
+
+}
